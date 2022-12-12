1dd2c2442f6306313659e2b7a953dcdc5f28c9ba
alirrah <alir.rah81@gmail.com>
2022-12-13 01:54:09 +0330

    fix buges

--- main.cpp
+++ main.cpp
@@ -12,31 +12,32 @@ int main()
     {
         system("cls");
         showMenu();
-        cout << "Option : ";
+        cout << "Option: ";
         cin >> option;
         switch (option)
         {
         case 1:
-            cout << "Balance is : " << balance << " $" << endl;
+            cout << "Balance is: " << balance << " $" << endl;
             break;
 
         case 2:
-            cout << "Deposit amount : ";
+            cout << "Deposit amount: ";
             double depositAmount;
             cin >> depositAmount;
             balance += depositAmount;
             break;
 
         case 3:
-            cout << "Withdraw amount : ";
+            cout << "Withdraw amount: ";
             double withdrawAmount;
             cin >> withdrawAmount;
             if (withdrawAmount <= balance)
-                balance -= depositAmount;
+                balance -= withdrawAmount;
             else
                 cout << "Not enough money" << endl;
             break;
         }
+        system("pause");
     } while (option != 4);
     return 0;
 }
@@ -47,6 +48,6 @@ void showMenu()
     cout << "1. Check balance" << endl;
     cout << "2. Deposit" << endl;
     cout << "3. Withdraw" << endl;
-    cout << "4. Exit";
+    cout << "4. Exit" << endl;
     cout << "************************************" << endl;
 }
